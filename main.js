fbea3e2f539fd182e9ee3eea8d50c8fd5992610a
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-02 09:32:08 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 300,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
